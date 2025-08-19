0369a00e8728d59a583a9f8b3d6c55b0e553b009
Roland Lindh NTI <118815577+RolandLindhNTI@users.noreply.github.com>
2025-08-19 12:14:27 +0200

    Update model.rb

--- model.rb
+++ model.rb
@@ -1,3 +1,12 @@
+    module Model    
+    require 'sinatra'
+    require 'sinatra/reloader'
+    require 'slim'
+    require 'sqlite3'
+
+    
+    
+    
     def database()
         db = SQLite3::Database.new("db/project_zero.db")
         db.results_as_hash = true

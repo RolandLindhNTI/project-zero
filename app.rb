33ee07f1ef6bd094bb0d80ea8fa8fcd3357bf742
Roland Lindh NTI <118815577+RolandLindhNTI@users.noreply.github.com>
2025-08-19 15:59:27 +0200

    broken code

--- app.rb
+++ app.rb
@@ -4,8 +4,18 @@ require "sqlite3"
 require "sinatra/reloader"
 require 'sinatra/flash' 
 require_relative 'model.rb'
+require 'debug'
 
 include Model
+
+
+
+configure do
+    enable :sessions
+    set :session_secret, "supersecret"
+
+end
+
 
 before do
     @db = database()
@@ -22,33 +32,37 @@ get('/') do
 end
 
 get '/game' do
+    puts session[:attempts]
+    puts session[:score]
   students = @db.execute("SELECT * from TE4")
 
   correct_student = students.shuffle.first
   incorrect_students = (students - [correct_student]).sample(2)
   options = ([correct_student] + incorrect_students).shuffle
-
+    @attempts = session[:attempts]
+    @score = session[:score]
   slim :game, locals:{correct_student: correct_student, options: options}
 end
 
 post '/answer' do
     students = @db.execute("SELECT * from TE4")
-    session[:attempts] ||= 0
-    session[:score] ||= 0
     id = params[:id].to_i
     correct_id = params[:correct_id].to_i
-
-
-
-    if students.length >= session[:attempts]
-
-        if correct_id == id 
-            session[:score] += 1
-        else
-            flash[:notice] = "wrong answer!"
-        end
+    if session[:attempts].nil? && session[:score].nil?
+        session[:attempts] = 0
+        session[:score] = 0
     end
-    session[:attempts] += 1
-    slim :game, locals:{score: session[:score], attempts: session[:attempts]}
+    if students.length >= session[:attempts]
+        if correct_id == id 
+            p session[:score]
+            session[:score] = 150
+            p session[:score]
+        end
+        p session[:score]
+    end
+    p "end of line"
+    p session[:attempts]
+    session[:attempts] = 100
+    p session[:attempts]
     redirect('/game')
 end

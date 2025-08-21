fe786faa0f141655df99fbcd652843eb09ffb1ce
Emanuel Hanner <emanuel.hanner@icloud.com>
2025-08-21 10:29:42 +0200

    Spaced code

--- app.rb
+++ app.rb
@@ -48,32 +48,39 @@ post '/answer' do
     if session[:time] == nil
       session[:time] = Time.now.to_i
     end
+
     students = @db.execute("SELECT * from game_class")
+
     id = params[:id].to_i
     correct_id = params[:correct_id].to_i
+
     if session[:attempts].nil? && session[:score].nil?
         session[:attempts] = students.length
         session[:attempts_real] = 0
         session[:score] = 0
     end
-        if students.length >= session[:attempts_real]
-            if correct_id == id 
-                if  session[:score] < session[:attempts]
-                    session[:score] += 1
-                end
 
-                
+    if students.length >= session[:attempts_real]
+        if correct_id == id 
+            if  session[:score] < session[:attempts]
+                session[:score] += 1
             end
 
+            
         end
+
+    end
+
     session[:attempts_real] += 1
     @db.execute("UPDATE game_class SET bild = NULL WHERE id = ?", correct_id)
+
     if students.length == session[:attempts_real]
 
         session[:time] = Time.now.to_i - session[:time]
 
       redirect('/results')
     end
+
     redirect('/game')
 end
 
@@ -81,22 +88,27 @@ get '/results' do
   @time = session[:time].to_i
   @attempts = session[:attempts]
   @score = session[:score]
+
   slim :results
 end
 
 post '/results' do
   name = params[:player_name]
+
   @time = session[:time].to_i
   @attempts = session[:attempts]
   @score = session[:score]
   @db.execute("INSERT INTO leaderboard (namn,score,time) VALUES (?,?,?)", [name,@score,@time])
+
   redirect '/restart'
 end
 
 get '/restart' do
   session.clear
+
   @db.execute("DELETE FROM game_class")
   @db.execute("INSERT INTO game_class SELECT * FROM TE4")
+  
   redirect '/'
 end
 
